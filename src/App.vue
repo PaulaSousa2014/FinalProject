0663244975ec5c0c73f6ef686509b5e5d756d4b4
PaulaSousa2014 <110301198+PaulaSousa2014@users.noreply.github.com>
2022-10-19 13:31:20 +0200

    proyecto preparado

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,11 @@
     <router-view></router-view>
 </template>
 <script setup>
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
+
+console.log(supabase)
 
 </script>
 <style scoped>
